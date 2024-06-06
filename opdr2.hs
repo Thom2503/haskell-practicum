548fe4071ee42b6fe3444253dc10f944ac6fe1fc
Douwe Zumker <Douwezumker@gmail.com>
2024-06-06 14:47:26 +0200

    verder aan de opdracht

--- opdr2.hs
+++ opdr2.hs
@@ -5,18 +5,27 @@ euclid 0 y = y
 euclid x y = euclid (y `mod` x) x
 
 -- vragen
+egcd' :: Integer -> Integer -> (Integer, Integer, Integer)
+egcd' 0 b = (b, 0, 1)
+egcd' a b =
+  let (g, s, t) = egcd' (b `mod` a) a
+   in (g, t - (b `div` a) * s, s)
+
 egcd :: Integer -> Integer -> (Integer, Integer, Integer)
-egcd 0 b = (b, 0, 1)
 egcd a b =
-    let (g, s, t) = egcd (b `mod` a) a
-        s' = if s < 0 then s `mod` g else s
-        t' = if t < 0 then t `mod` g else t
-    in (g, t' - (b `div` a) * s', s')
+  let (g, s, t) = egcd' a b
+   in if s < 0 then (g, s + b, t) else (g, s, t)
 
 genkeys :: Integer -> Integer -> (Integer, Integer, Integer)
 genkeys p q =
-   let m = p * q
-       m' = (p - 1) * (q - 1)
-       e = 5 -- vragen
-       d = (e ^ (-1)) * (e `mod` m')
-    in (e, d, m)
+  let m = p * q
+      m' = (p - 1) * (q - 1)
+      e = head [x | x <- [2..m'], euclid x m' == 1]
+      (_,d,_) = egcd e m'
+   in (e, d, m)
+
+rsaencrypt::(Integer,Integer)->Integer->Integer
+rsaencrypt (e,m) x = mod (x^e) m
+
+rsadecrypt::(Integer,Integer)->Integer->Integer
+rsadecrypt (d,m) x = mod (x^d) m
