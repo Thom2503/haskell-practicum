a6665be0f5f9620b0667ace3feecf59b171b7ebe
Douwe Zumker <Douwezumker@gmail.com>
2024-06-06 15:00:56 +0200

    bijna klaar

--- opdr2.hs
+++ opdr2.hs
@@ -24,8 +24,32 @@ genkeys p q =
       (_,d,_) = egcd e m'
    in (e, d, m)
 
+
+-- Oefenen met genkeys rsaencrypt en rsadecrypt
+--ghci> import Data.Char
+--ghci> genkeys 7 67
+--(5,317,469)
+--ghci> ord 'k'
+--107
+--ghci> rsaencrypt (5,469) 107
+--81
+--ghci> rsadecrypt (317,469) 81
+--107
+--ghci> chr 81
+--'Q'
+--ghci> chr 107
+--'k'
+
+
 rsaencrypt::(Integer,Integer)->Integer->Integer
 rsaencrypt (e,m) x = mod (x^e) m
 
 rsadecrypt::(Integer,Integer)->Integer->Integer
 rsadecrypt (d,m) x = mod (x^d) m
+
+-- Opdracht 5
+-- Als Alice een bericht veilig wil sturen naar Bob kan zij het beste,
+-- eerst het bericht met de public key van bob encrypten en dan met haar
+-- eigen private key encrypten. En dan aan de kant van Bob moet bob eerst de public
+-- key van Alice gebruiken om het te decrypten en dan zijn eigen private key gebruiken
+-- om het nog een keer te decrypten.
